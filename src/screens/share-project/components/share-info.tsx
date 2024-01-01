edcf4e5cb4f467d8b42d35fc05da1bed5aecc210
Santhosh kumar <15356760+santhosh1993@users.noreply.github.com>
2024-01-01 19:11:31 +0530

    fix: updated styles

--- src/screens/share-project/components/share-info.tsx
+++ src/screens/share-project/components/share-info.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import { ShareProjectCardInterFace } from '../share-project.interface';
 import QRCode from 'react-native-qrcode-svg';
 import { Text } from '@common/text';
@@ -7,16 +7,23 @@ import { FontSize } from '@common/font';
 
 export const ShareInfo = ({redirectionUrl, ...shareCard}: ShareProjectCardInterFace) => {
   return (
-    <View style={{paddingHorizontal: 16, paddingTop: 8,paddingBottom: 0, alignItems: 'center'}}>
-      <Text style={{fontSize: FontSize.xxlarge, paddingBottom: 12}}>{shareCard.rconName}</Text>
+    <View style={styles.container}>
+      <Text style={styles.rconName}>{shareCard.rconName}</Text>
       <QRCode
       value={redirectionUrl}
       size={200}
       />
-      <View style={{flexDirection:'row', paddingTop: 4, justifyContent: 'space-between', width: '100%'}}>
-        <Text style={{fontSize: FontSize.xlarge}}>{shareCard.userName}</Text>
-        <Text style={{fontSize: FontSize.xlarge}}>{shareCard.phoneNo}</Text>
+      <View style={styles.userContainer}>
+        <Text style={styles.userText}>{shareCard.userName}</Text>
+        <Text style={styles.userText}>{shareCard.phoneNo}</Text>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {paddingHorizontal: 16, paddingTop: 8,paddingBottom: 0, alignItems: 'center'},
+  rconName: {fontSize: FontSize.xxlarge, paddingBottom: 12},
+  userContainer: {flexDirection:'row', paddingTop: 4, justifyContent: 'space-between', width: '100%'},
+  userText: {fontSize: FontSize.xlarge}
+})
