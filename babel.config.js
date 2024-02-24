7707bc035b9b92f81c581456b8de0c21af9c1cb7
Santhosh kumar <15356760+santhosh1993@users.noreply.github.com>
2024-02-24 22:05:09 +0530

    feat: inital upgrade

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ const tsconfig = require('./tsconfig.json');
 const paths = tsconfig.compilerOptions.paths;
 
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
       'module-resolver',
